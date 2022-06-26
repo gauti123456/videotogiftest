1361096d82c3770cc695d9d9ed68833288ed4274
gauti123456 <geekygautam1997@gmail.com>
2022-06-26 15:26:04 +0530

    second commit

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express')
 
 const app = express()
 
+app.use(function (req, res, next) {
+    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+    res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+    next();
+});
+
 const PORT = process.env.PORT || 5000
 
 app.get('/',(req,res) => {
